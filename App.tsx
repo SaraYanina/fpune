87ed415eefd15074cff2b9f12a64b21166f6cf97
SaraYanina <sarayaninaguthi20@gmail.com>
2024-09-09 20:13:50 -0400

    prueba

--- App.tsx
+++ App.tsx
@@ -1,118 +1,54 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from "react";
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  
+}from 'react-native';
+//import BasicComponents from "./src/components/BasicComponents";
+//import TextInANest from "./src/components/TextComponents";
+//import TextInputExample from "./src/components/TextInputComponents";
+//import DisplayAnImage from "./src/components/ImageComponents";
+//import FlatListExample from "./src/components/FlatListComponents";
+//import Prensable from "./src/components/PressableComponents";
+//import UseStateComponents from "./src/components/UseState";
+//import ListmateriaComponents from "./src/components/Calificaciones";
+//import ListalumnosComponents from "./src/components/ListaAlumnos";
+import Home from "./android/app/src/react-native-elements/Home";
+import { NavigationContainer, NavigationContainerProps } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+const Stack = createNativeStackNavigator ();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const App = () => {
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  return(
+    //<SafeAreaView style={styles.safeArea}>
+      
+       {/*<BasicComponents></BasicComponents>
+     <TextInANest></TextInANest>
+     <TextInputExample></TextInputExample>
+     <DisplayAnImage></DisplayAnImage>
+     <FlatListExample></FlatListExample>
+     <Prensable></Prensable>
+     <UseStateComponents></UseStateComponents>
+     <ListmateriaComponents></ListmateriaComponents>
+      <ListalumnosComponents></ListalumnosComponents>
+     */}
+    //</SafeAreaView>
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  safeArea: {
+    flex: 1,
+    backgroundColor: 'white', // Asegúrate de que el fondo sea visible
   },
 });
+
 
 export default App;
---
+++ android/app/src/react-native-elements/Home.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Text } from 'react-native'
+
+const Home = () =>{
+    return(
+        <text> Pagina Principal</text>
+    )
+}
+export default Home
